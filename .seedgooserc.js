20259c8fda27bcbb1aba520b1160fba5ee784f2d
joseph <jcastrigno@gmail.com>
2023-09-20 18:05:49 -0600

    Module Four Assignment

---
+++ .seedgooserc.js
@@ -0,0 +1,13 @@
+// module.exports = {
+//     modelBaseDirectory: 'app_server/models', // model directory name
+//     models: '**/*.js', // model matcher
+//     data: 'data', // data directory name
+//     db: 'mongodb://localhost:27017/DB-URL' // db connection url
+// }
+
+module.exports = {
+    modelBaseDirectory:"app_server/database/models",
+    models: ["*.js", "!db.js"],
+    data: "data",
+    db: "mongodb://localhost:27017/travlr"
+}
